763a2ae52cc9673082c61e9312ec35a783930937
Woonohyo <woonohyo@naver.com>
2020-06-10 22:06:44 +0900

    Project 01 - Create project and add image resources

---
+++ GoodAsOldPhones/GoodAsOldPhones/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoodAsOldPhones
+//
+//  Created by Woonohyo on 2020/06/09.
+//  Copyright © 2020 woonohyo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
