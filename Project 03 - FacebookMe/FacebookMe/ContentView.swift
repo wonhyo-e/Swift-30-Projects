6d6b0f4336aebd7005554a431347ab19ea56f4d8
Woonohyo <woonohyo@naver.com>
2020-06-13 22:04:34 +0900

    Project 03 - Initial commit

---
+++ Project 03 - FacebookMe/FacebookMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FacebookMe
+//
+//  Created by Woonohyo on 2020/06/13.
+//  Copyright © 2020 woonohyo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
